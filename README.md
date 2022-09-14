b1c46924e3be9f4cd03afabda7649d5bb93bbf7c
Tamás K. Papp <tkpapp@gmail.com>
2022-09-14 09:37:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ReproducibleLiteratePage.jl
+
+![lifecycle](https://img.shields.io/badge/lifecycle-experimental-orange.svg)<!--
+[![build](https://github.com/tpapp/ReproducibleLiteratePage.jl/workflows/CI/badge.svg)](https://github.com/tpapp/ReproducibleLiteratePage.jl/actions?query=workflow%3ACI)
+[![codecov.io](http://codecov.io/github/tpapp/ReproducibleLiteratePage.jl/coverage.svg?branch=master)](http://codecov.io/github/tpapp/ReproducibleLiteratePage.jl?branch=master)
+
+A simple package to generate a markdown file from Julia source marked up with [Literate.jl](https://github.com/fredrikekre/Literate.jl), with the source, project and manifest files added in a `tar` archive linked at the bottom of the page.
+
+I am using it for my [blog](https://www.tamaspapp.eu), and currently do not plan to register it, but feel free to ask.
---
+++ src/ReproducibleLiteratePage.jl
@@ -0,0 +1,64 @@
+"""
+Placeholder for a short summary about ReproducibleLiteratePage.
+"""
+module ReproducibleLiteratePage
+
+export compile_directory
+
+import Literate
+import Tar
+import Pkg
+
+"Default source file within the directory."
+const DEFAULT_SOURCE = "index.jl"
+
+"Default archive."
+const DEFAULT_ARCHIVE = "source.tar"
+
+"<div> class for the source footer."
+const DIV_CLASS = "source_footer"
+
+"""
+$(SIGNATURES)
+
+Put the source, project, and manifest files in `archive_name` using `tar`.
+"""
+function tar_files(archive_name, dir; source = DEFAULT_SOURCE)
+    Tar.create(dir, archive_name) do p
+        relpath(p, dir) in ["Project.toml", "Manifest.toml", source]
+    end
+end
+
+"""
+$(SIGNATURES)
+
+Generate a footer for the page, wrapping in [`DIV_CLASS`](@ref).
+"""
+function generate_footer(archive_name)
+    """
+    <div class="$(DIV_CLASS)">
+    This page was generated using [Literate.jl](https://github.com/fredrikekre/Literate.jl).
+    Download [the source, project, and manifest]($(archive_name)).
+    </div>
+    """
+end
+
+"""
+$(SIGNATURES)
+
+Compile `joinpath(dir, source)` using `Literate.markdown`, put the source, manifest, and
+project files in an archive, add it as a footer.
+"""
+function compile_directory(dir; source = DEFAULT_SOURCE, archive = DEFAULT_ARCHIVE)
+    src_file = joinpath(dir, source)
+    tar_file = joinpath(dir, archive)
+    tar_files(tar_file, dir; source)
+    Pkg.activate(dir)
+    @show dir
+    Literate.markdown(src_file, dir;
+                      postprocess = s -> s * generate_footer(tar_file),
+                      execute = true,
+                      config = false)
+end
+
+end # module
